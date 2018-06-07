50a03ef1b36a583a7b7d49f2e058f028f6c07cdf
TheRealMeekey <therealmeekey@gmail.com>
2018-06-07 17:36:58 +0300

    edit urls

--- ams/urls.py
+++ ams/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
